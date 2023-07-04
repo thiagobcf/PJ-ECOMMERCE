c79c19c313e2d6b4b9376af193ddaf5f21ab003c
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-07-04 19:29:36 -0300

    criando os models Produto e Variação

--- loja/settings.py
+++ loja/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'produto',
+
     "debug_toolbar",
 ]
 
@@ -49,7 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'loja.urls'
--- loja/urls.py
+++ loja/urls.py
@@ -21,6 +21,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    path("__debug__/", include("debug_toolbar.urls")),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('_debug_/', include(debug_toolbar.urls))
+    ] + urlpatterns
