46fbd25a27996df0b2a643ccaf8f8255ab41c66c
Thiago Fonseca <thiagobcfonseca@gmail.com>
2023-07-07 12:56:52 -0300

    detalhes do produto

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 
@@ -9,10 +10,14 @@ class ListaProdutos(ListView):
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
     paginate_by = 2
+    ordering = ['-id']
 
 
-class DetalheProduto(View):
-    ...
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class Adicionaraocarrinho(View):
